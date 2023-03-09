4f2d1c3271d21ef2c5587a236442b18778e30116
Abhisheklikho <111559519+Abhisheklikho@users.noreply.github.com>
2023-03-09 11:58:58 +0530

    solution

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your JS code here. If required.
+function input1() {
+    var first_value = prompt("Enter the first value");
+    return parseInt(first_value);
+  }
+  
+  function input2() {
+    var last_value = prompt("Enter the second value");
+    return parseInt(last_value);
+  }
+  
+  function finalResult() {
+    var first_value = input1();
+    var last_value = input2();
+    var result = (first_value * 2) + last_value;
+    alert("The final result is: " + result);
+  }
+  
+  finalResult();
+  
